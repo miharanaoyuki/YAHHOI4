32029a3733a32978ec23126f2316b3e40fc2c1d2
miharanaoyuki <89573107+miharanaoyuki@users.noreply.github.com>
2022-11-14 22:42:05 +0900

    mihara
    ステージ4の、触れるとダメージを喰らうバーの作成&動作確認

---
+++ YAHHOI_4/Assets/NAO/Scripts/FPS1.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+}
